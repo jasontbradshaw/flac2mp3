3fc5bf6bb71ea25c1f764777c6defa4ea50b67cf
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-04-12 02:34:07 -0500

    moved util file into main file; there wasn't really enough in there to bother having two files in the first place

--- flac2mp3.py
+++ flac2mp3.py
@@ -5,7 +5,73 @@ import re
 import subprocess as sp
 import multiprocessing as mp
 
-import util
+def get_changed_file_ext(fname, ext):
+    """
+    Transforms the given filename's extension to the given extension.  'ext' is
+    the new extension, 'fname' is the file name transformation is to be done on.
+    """
+
+    # determine the new file name (old file name minus the extension if it had
+    # one, otherwise just the old file name with new extension added).
+    new_fname = fname
+
+    # if we have a file extension, strip the final one and get the base name
+    if len(fname.rsplit(".", 1)) == 2:
+        new_fname = fname.rsplit(".", 1)[0]
+
+    # add the new extension
+    new_fname += ext
+
+    return new_fname
+
+def walk_dir(d, follow_links=False):
+    """
+    Returns all the file names in a given directory, including those in
+    subdirectories.  if 'follow_links' is True, symbolic links will be followed.
+    This option can lead to infinite looping since the function doesn't keep
+    track of which directories have been visited.
+    """
+
+    # attempt to get the files in the given directory, returning an empty list
+    # if it failed for any reason.
+    contents = []
+    try:
+        contents = os.listdir(d)
+    except OSError:
+        return []
+
+    # add original directory name to every listed file
+    contents = map(lambda x: os.path.join(d, x), contents)
+
+    # normalize all file names
+    contents = map(os.path.abspath, contents)
+
+    # add all file names to a list recursively
+    new_files = []
+    for f in contents:
+        # skip links if specified
+        if not follow_links and os.path.islink(f):
+            continue
+
+        # add all the files under a dir to the list
+        if os.path.isdir(f):
+            new_files.extend(walk_dir(f, follow_links=follow_links))
+        # add files to the list
+        else:
+            new_files.append(f)
+
+    return new_files
+
+def get_filetype(fname):
+    """
+    Gets the file type of the given file as a MIME string and returns it.
+    """
+
+    # brief output, MIME version
+    file_args = ["file", "-b", "-i", fname]
+    p_file = sp.Popen(file_args, stdout=sp.PIPE)
+
+    return p_file.communicate()[0]
 
 def transcode(infile):
     """
@@ -21,7 +87,7 @@ def transcode(infile):
     flac_data = p_flac.communicate()[0]
 
     # get a new file name for the mp3 based on the input file's name
-    mp3_filename = util.get_changed_file_ext(infile, ".mp3")
+    mp3_filename = get_changed_file_ext(infile, ".mp3")
 
     # get the tags from the input file
     flac_tags = get_tags(infile)
@@ -95,12 +161,12 @@ if __name__ == "__main__":
     flacfiles = []
     for f in sys.argv[1:]:
         if os.path.isdir(f):
-            flacfiles.extend(util.walk_dir(f))
+            flacfiles.extend(walk_dir(f))
         else:
             flacfiles.append(f)
 
     # remove all non-flac files from the list
-    flacfiles = filter(lambda x: util.get_filetype(x).count("audio/x-flac") > 0,
+    flacfiles = filter(lambda x: get_filetype(x).count("audio/x-flac") > 0,
             flacfiles)
 
     # remove duplicates and sort resulting list
